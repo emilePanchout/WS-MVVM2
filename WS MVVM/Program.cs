a8908d7698b7cc48c25d90515b73102b745ae18b
emilePanchout <ping0yn76@gmail.com>
2021-11-19 09:18:15 +0100

    Ajoutez des fichiers projet.

---
+++ WS MVVM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WS_MVVM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
